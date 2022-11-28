e31b765b144786cfd19fcc27b21d555d14a72419
JessenReinhart <jessen_1206@yahoo.com>
2022-11-28 20:14:09 +0700

    feat(movies): sidebar color tweak

--- src/pages/Movies.vue
+++ src/pages/Movies.vue
@@ -11,19 +11,20 @@ const genres = ["Action", 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'History', 
             </h2>
         </div>
         <div class="flex flex-row gap-6">
-            <div class="flex flex-col rounded-xl bg-gradient-to-b from-slate-800 w-2/5 text-white h-max ">
-                <span class="text-lg font-bold py-6 px-4 border-b border-slate-600">Sort Result By</span>
-                <div class="border-b border-slate-600 flex flex-row">
-                    <select class="py-2 px-4 m-4 w-full rounded bg-slate-500">
+            <div
+                class="flex flex-col rounded-xl bg-gradient-to-b from-[#0E1723] to-[#1E232A]/0 w-2/5 text-white h-max ">
+                <span class="text-lg font-semibold py-6 px-8 border-b border-[#FFFFFF]/10">Sort Result By</span>
+                <div class="border-b border-[#FFFFFF]/10 flex flex-row">
+                    <select class="py-2 px-8 m-4 w-full rounded bg-slate-500">
                         <option selected>
                             Popularity
                         </option>
                     </select>
                 </div>
-                <span class="text-lg font-bold py-6 px-4 border-b border-slate-600">Genres</span>
+                <span class="text-lg font-semibold py-6 px-8 border-b border-[#FFFFFF]/10">Genres</span>
                 <div class="py-6 flex flex-col">
                     <label v-for="genre, key in genres" :key="key" :for="genre"
-                        class="flex flex-row space-between cursor-pointer w-full py-2 px-4 hover:bg-white/10 transition">
+                        class="flex flex-row space-between cursor-pointer w-full py-2 px-8 hover:bg-white/10 transition">
                         <span>
                             {{ genre }}
                         </span>
